c53adf1e08454823f642daf49d667bbe412ac6b2
MattiaVolonterio <volonteriomattia@gmail.com>
2024-07-16 17:20:40 +0200

    fix

--- database/seeders/TeamSeeder.php
+++ database/seeders/TeamSeeder.php
@@ -17,23 +17,6 @@ class TeamSeeder extends Seeder
      */
     public function run()
     {
-
-        // $client = new http\Client;
-        // $request = new http\Client\Request;
-
-        // $request->setRequestUrl('https://v3.football.api-sports.io/countries');
-        // $request->setRequestMethod('GET');
-        // $request->setHeaders(array(
-        //     'x-rapidapi-host' => 'v3.football.api-sports.io',
-        //     'x-rapidapi-key' => 'XxXxXxXxXxXxXxXxXxXxXxXx'
-        // ));
-
-        // $client->enqueue($request)->send();
-        // $response = $client->getResponse();
-
-        // echo $response->getBody();
-
-
         try {
             $headers = [
                 'x-rapidapi-host' => 'v3.football.api-sports.io',
